1a6958b941991ff03faf607fd30a72e494b93b83
Icaro Andrade <iba1987@gmail.com>
2021-10-16 18:48:56 -0300

    update backend url

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,7 +1,7 @@
 const streams = [];
 
 (async () => {
-  const res = await axios.get("http://localhost:5100/api");
+  const res = await axios.get("https://twitch-rpg-in-pt.herokuapp.com/api");
   streams.push(...res.data);
 
   updateOnlineLiveCounter(streams.length);
