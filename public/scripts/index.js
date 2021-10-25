11c99ed224862d694f08cbc89f97892eb3a6524e
Icaro Andrade <iba1987@gmail.com>
2021-10-25 17:32:57 -0300

    refactor card creation

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,7 +5,7 @@ const streams = [];
   streams.push(...res.data);
 
   updateOnlineLiveCounter(streams.length);
-  createCards(streams);
+  createCards2(streams);
 })();
 
 function updateTimer(user_idx) {
@@ -18,9 +18,7 @@ function updateTimer(user_idx) {
   minutes = seconds === 0 ? (minutes + 1 === 60 ? 0 : minutes + 1) : minutes;
   hours = minutes === 0 && seconds === 0 ? hours + 1 : hours;
 
-  streams[user_idx].hours = hours;
-  streams[user_idx].minutes = minutes;
-  streams[user_idx].seconds = seconds;
+  updateTimeForUser(user_idx, hours, minutes, seconds);
 
   if (timerElement) {
     timerElement.textContent = `${hours === 0 ? "" : hours + ":"}${minutes
@@ -32,13 +30,11 @@ function updateTimer(user_idx) {
 function getOnlineTime(time, user_idx) {
   const started = new Date(time);
   const diff = Date.now() - started;
-  let hours = Math.floor(diff / 1000 / 60 / 60);
-  let minutes = Math.floor((diff / 1000 / 60) % 60);
-  let seconds = Math.floor(((diff / 1000) % 60) % 60);
+  const hours = Math.floor(diff / 1000 / 60 / 60);
+  const minutes = Math.floor((diff / 1000 / 60) % 60);
+  const seconds = Math.floor(((diff / 1000) % 60) % 60);
 
-  streams[user_idx].hours = hours;
-  streams[user_idx].minutes = minutes;
-  streams[user_idx].seconds = seconds;
+  updateTimeForUser(user_idx, hours, minutes, seconds);
 
   let timeOnline = `${hours === 0 ? "" : hours + ":"}${minutes
     .toString()
@@ -49,6 +45,13 @@ function getOnlineTime(time, user_idx) {
   return timeOnline;
 }
 
+function updateTimeForUser(user_idx, hours, minutes, seconds) {
+  streams[user_idx].hours = hours;
+  streams[user_idx].minutes = minutes;
+  streams[user_idx].seconds = seconds;
+}
+
+// [TODO] update h2 instead of create
 function updateOnlineLiveCounter(counter) {
   const h2 = document.createElement("h2");
   h2.className = "sub-header";
@@ -56,111 +59,107 @@ function updateOnlineLiveCounter(counter) {
   document.getElementById("header").append(h2);
 }
 
+let isOver = false;
+let animation;
+let cardClasses;
+
+function onMouseOver(element) {
+  if (!isOver) {
+    isOver = true;
+
+    const cardElement = element.querySelector(".card");
+    cardClasses = cardElement.className;
+    cardElement.className = `${cardClasses} cardMouseOver`;
+
+    const avatarElement = cardElement.querySelector(".avatar");
+    avatarElement.className = "avatar avatarMouseOver";
+
+    const userElement = cardElement.querySelector(".user");
+    addAnimation(userElement);
+  }
+}
+
+function onMouseLeave(element) {
+  if (isOver) {
+    console.log("leave");
+    isOver = false;
+
+    const cardElement = element.querySelector(".card");
+    cardElement.className = cardClasses;
+
+    const avatarElement = cardElement.querySelector(".avatar");
+    avatarElement.className = "avatar";
+
+    const userElement = cardElement.querySelector(".user");
+    if (userElement.scrollWidth > userElement.clientWidth) {
+      userElement.className = "user";
+      animation.cancel();
+    }
+  }
+}
+
+function addAnimation(element) {
+  if (element.scrollWidth > element.clientWidth) {
+    const animDuration = Math.max(
+      (1500 * (element.scrollWidth - element.clientWidth)) / 28,
+      750
+    );
+    animation = element.animate(
+      [
+        { transform: "translateX(0px)" },
+        {
+          transform: `translateX(-${
+            element.scrollWidth - element.clientWidth
+          }px)`,
+        },
+      ],
+      {
+        duration: animDuration,
+        iterations: Infinity,
+        direction: "alternate",
+        easing: "ease-in-out",
+      }
+    );
+  }
+}
+
 function createCards(streams) {
   const container = document.getElementById("streams-container");
-  streams.map(
-    (
-      {
-        user_name,
-        is_mature,
-        viewer_count,
-        title,
-        profile_img,
-        thumbnail_url,
-        user_id,
-        started_at,
-      },
-      idx
-    ) => {
-      const avatar = document.createElement("img");
-      avatar.src = profile_img;
-      avatar.className = "avatar";
 
-      const userDiv = document.createElement("div");
-      userDiv.className = "userDiv";
-      const user = document.createElement("p");
-      user.textContent = user_name;
-      user.className = "user";
-      userDiv.append(user);
-
-      const viewers = document.createElement("p");
-      viewers.textContent = viewer_count;
-      viewers.className = "viewers";
-
-      const streamTitle = document.createElement("p");
-      streamTitle.textContent =
-        title.length >= 30 ? `${title.substr(0, 27)}...` : title;
-      streamTitle.className = "title";
-
-      const thumb = document.createElement("img");
-      thumb.src = thumbnail_url
-        .replace("{width}", "400")
-        .replace("{height}", "240");
-      thumb.className = "thumb";
-
-      const onlineTime = document.createElement("p");
-      onlineTime.textContent = getOnlineTime(started_at, idx);
-      onlineTime.className = "timer";
-      onlineTime.id = `time${idx}`;
-
-      const cardFooter = document.createElement("div");
-      cardFooter.className = "card-footer";
-      cardFooter.append(onlineTime, viewers);
-
-      const card = document.createElement("div");
-      card.className = is_mature ? "card mature" : "card";
-      card.id = user_id;
-      card.append(avatar, userDiv, streamTitle, thumb, cardFooter);
-
-      const link = document.createElement("a");
-      link.href = `https://twitch.tv/${user_name}`;
-      link.target = "_blank";
-      link.className = "text-light";
-      link.append(card);
-      let isover = false;
-      let animation;
-      link.addEventListener("mouseover", (e) => {
-        if (!isover) {
-          const userElement = e.currentTarget
-            .querySelector(".card")
-            .querySelector(".user");
-          if (userElement.scrollWidth > userElement.clientWidth) {
-            const animDuration = Math.max(
-              (1500 * (userElement.scrollWidth - userElement.clientWidth)) / 28,
-              750
-            );
-            isover = true;
-            animation = userElement.animate(
-              [
-                { transform: "translateX(0px)" },
-                {
-                  transform: `translateX(-${
-                    userElement.scrollWidth - userElement.clientWidth
-                  }px)`,
-                },
-              ],
-              {
-                duration: animDuration,
-                iterations: Infinity,
-                direction: "alternate",
-                easing: "ease-in-out",
-              }
-            );
-          }
-        }
-      });
-      link.addEventListener("mouseleave", (e) => {
-        const userElement = e.currentTarget
-          .querySelector(".card")
-          .querySelector(".user");
-        if (userElement.scrollWidth > userElement.clientWidth) {
-          isover = false;
-          userElement.className = "user";
-          animation.cancel();
-        }
-      });
-
-      container.appendChild(link);
-    }
-  );
+  container.innerHTML = `${streams
+    .map(
+      (stream, idx) => `
+      <a class="text-light" href="https://twitch.tv/${
+        stream.user_name
+      }" target="_blank" onmouseover="onMouseOver(this)" onmouseleave="onMouseLeave(this)">
+        <div class="card ${stream.is_mature ? "mature" : ""}">
+          <img class="avatar" src=${stream.profile_img} />
+          <div class="user-div">
+            <p class="user">
+              ${stream.user_name}
+            </p>
+          </div>
+          <p class="title">
+            ${
+              stream.title.length >= 30
+                ? `${stream.title.substr(0, 27)}...`
+                : stream.title
+            }
+          </p>
+          <img class="thumb" src=${stream.thumbnail_url
+            .replace("{width}", "400")
+            .replace("{height}", "240")} />
+          <div class="card-footer">
+            <p id=time${idx} class="timer">${getOnlineTime(
+        stream.started_at,
+        idx
+      )}</p>
+            <p class="viewers">${stream.viewer_count}</p>
+          </div>
+        </div>
+      </a>
+    `
+    )
+    .toString()
+    .replace(/,/gi, "")}`;
 }
