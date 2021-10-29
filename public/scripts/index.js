1a5f95e0f9bc695c84eacb948f45c6db41909802
Icaro Andrade <iba1987@gmail.com>
2021-10-29 14:13:59 -0300

    fix mobile card

--- public/scripts/index.js
+++
@@ -1,9 +0,0 @@
-const streams = [];
-
-(async () => {
-  const res = await axios.get("https://twitch-rpg-in-pt.herokuapp.com/api");
-  streams.push(...res.data);
-
-  updateOnlineLiveCounter(streams.length);
-  createCards(streams);
-})();
